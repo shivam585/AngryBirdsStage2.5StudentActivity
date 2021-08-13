8270af31e48ea9bb91ba28dadc0e1d1f119f67d0
shivam585 <85876404+shivam585@users.noreply.github.com>
2021-08-13 17:31:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -44,6 +44,8 @@ function draw(){
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+    
+    text("shivam",400,400)
     box1.display();
     box2.display();
     ground.display();
